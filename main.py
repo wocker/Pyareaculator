e743dd8391e06f4808fe054592e963f95b8ba1dd
wocker <johel41@gmail.com>
2012-02-14 13:41:54 -0500

    Prueba

---
+++ main.py
@@ -0,0 +1,28 @@
+import math
+print "Pyareacalculator: Calcula el area de una figura geometrica"
+
+base_triangulo = float(raw_input("Introduzca la base:"))
+altura_triangulo = float(raw_input("Introduzca la altura:"))
+area_triangulo = base_triangulo * altura_triangulo / 2.0
+print area_triangulo
+triangulo = area_triangulo 
+
+radio_circulo = float(raw_input("Introduzca el radio:"))
+area_circulo = radio_circulo * pi
+circulo = area_circulo
+ 
+print "Que area deseas calcular"
+ 
+opcion = z
+while < "a" and opcion > "b" 
+        print "a) El area de un triangulo"
+        print "b) El area de un circulo"
+       
+opcion = raw_input("Elige la figura geometrica")
+        if opcion == "a"
+                print triangulo
+        elif opcion == "b"
+                print circulo
+        else
+        print "Por favor elige una figura geometrica"
+ 
