8f359d91b8c9a4b5e9e99531e93fc02c7b78c83a
wocker <johel41@gmail.com>
2012-02-14 14:42:57 -0500

    El programa ya no tiene ningun bug, creo yo.

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
 import math
-print "Pyareacalculator: Calcula el area de una figura geometrica"
-
-base_triangulo = float(raw_input("Introduzca la base:"))
-altura_triangulo = float(raw_input("Introduzca la altura:"))
-area_triangulo = base_triangulo * altura_triangulo / 2.0
-print area_triangulo
-triangulo = area_triangulo 
-
-radio_circulo = float(raw_input("Introduzca el radio:"))
-area_circulo = radio_circulo * pi
-circulo = area_circulo
+print "Pyareacalculator: Calcula el area de una figura geometrica\n"
+print "Que area deseas calcular \n"
  
-print "Que area deseas calcular"
- 
-opcion = z
-while < "a" and opcion > "b" 
+opcion = "z"
+while opcion != "a"  and opcion != "b":
         print "a) El area de un triangulo"
         print "b) El area de un circulo"
        
-opcion = raw_input("Elige la figura geometrica")
-        if opcion == "a"
-                print triangulo
-        elif opcion == "b"
-                print circulo
-        else
-        print "Por favor elige una figura geometrica"
+        opcion = raw_input("Elige la figura geometrica:\n")
+        if opcion == "a":
+			base_triangulo = float(raw_input("Introduzca la base:"))
+			altura_triangulo = float(raw_input("Introduzca la altura:"))
+			area_triangulo = base_triangulo * altura_triangulo / 2.0
+			print area_triangulo
+        elif opcion == "b":
+			radio_circulo = float(raw_input("Introduzca el radio:"))
+			area_circulo = radio_circulo * 3.1416
+			print area_circulo
+        else:
+			print "Por favor elige una figura geometrica"
  #Comentario
+ 
