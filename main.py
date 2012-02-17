d48b7a90b5aea75b4cc25b32263fefad948fa57d
wocker <johel41@gmail.com>
2012-02-17 07:31:41 -0500

    Se actualizo el programa con mas funciones y otros arreglos graficos.

--- main.py
+++ main.py
@@ -1,40 +1,35 @@
-#Importa el modulo math de Python
 import math
-#Imprimo el titulo de la aplicacion
 print "Pyareacalculator: Calcula el area de una figura geometrica\n"
-#Imprimo el subtitulo de la pregunta
 print "Que area deseas calcular \n"
 #Defino la variable opcion con el valor de string z
 opcion = "z"
 #Inicio un while para que el usuario ingrese la opcion
 while opcion != "a"  and opcion != "b":
-		#Imprimo la opcion del area de un triangulo
-        print "a) El area de un triangulo"
-        #Imprimo la opcion del area de un triangulo
-        print "b) El area de un circulo"
-        #Formulario para que los usuarios ingresen la figura geometria de la que desean sacar el area
-        opcion = raw_input("Elige la figura geometrica:\n")
-        #Hago la comprobacion si el usuario ingreso la opcion a
-        if opcion == "a":
-			#Formuario para que los usuarios ingresen la base del triangulo
+		print "a) El area de un triangulo"
+		print "b) El area de un circulo"
+		print "c) El area de un rectangulo"
+		print "d) El area de un Paralelogramo"
+		opcion = raw_input("Elige la figura geometrica:\n")
+        
+		if opcion == "a":
 			base_triangulo = float(raw_input("Introduzca la base:"))
-			#Formulario para que los usuarios ingresen la altura del triangulo
 			altura_triangulo = float(raw_input("Introduzca la altura:"))
-			#Defino la variable area_triangulo que es la base_triangulo por la altura_triangulo entre dos
 			area_triangulo = base_triangulo * altura_triangulo / 2.0
-			#Imprimo la variable area_triangulo
-			print area_triangulo
-		#Hago la comprobacion si el usuario ingreso la opcion b
-        elif opcion == "b":
-			#Formulario para que los usuarios ingresen el radio del circulo
+			print "El area del triangulo es:", +area_triangulo
+			
+		elif opcion == "b":
 			radio_circulo = float(raw_input("Introduzca el radio:"))
-			#Defino la variable area_circulo que calcula el area del circulo basado en el radio_circulo por la constante pi
-			area_circulo = radio_circulo * 3.1416
-			#Imprimo la variable area_circulo
-			print area_circulo
-		#Si la opcion ingresada por el usuario no es ni a ni b, hacemos dicha comprobacion
-        else:
-			#Imprimo un mensaje para los usuarios que no ingresaron ninguna opcion valida
-			print "Por favor elige una figura geometrica"
-#Fin del programa
- 
+			area_circulo = radio_circulo * 3.1415926535897932384626433832795
+			print "El area del circulo es:", +area_circulo
+			
+		elif opcion == "c":
+			base_rectangulo = float(raw_input("Introduzca la base:"))
+			altura_rectangulo = float(raw_input("Introduzca la altura:"))
+			area_rectangulo = base_rectangulo * altura_rectangulo
+			print "El area del rectangulo es:", +area_rectangulo
+			
+		elif opcion == "d":
+			base_paralelogramo = float(raw_input("Introduzca la base:"))
+			altura_paralelogramo = float(raw_input("Introduzca la altura:"))
+			area_paralelogramo = base_paralelogramo * altura_paralelogramo
+			print "El area del paralelogramo es:", +area_paralelogramo
